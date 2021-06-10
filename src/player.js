b66c48497c006fdf0f95eebac176192a8560e0a2
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-06-10 14:55:14 -0600

    Add player methods

--- src/player.js
+++ src/player.js
@@ -4,4 +4,14 @@ class Player {
     this.token = playerInfo.token;
     this.wins = playerInfo.wins || 0;
   }
+
+  saveWinsToStorage() {
+    var payload = JSON.stringify(this.wins)
+    localStorage.setItem(`${this.id}`, payload)
+  }
+
+  retrieveWinsFromStorage() {
+    var response = localStorage.getItem(`${this.id}`)
+    this.wins = JSON.parse(response)
+  }
 }
