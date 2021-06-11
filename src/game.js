7f44e7f11187c36e77a6697695f8d4d1789c2b40
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-06-11 10:18:16 -0600

    Add function for player turn order

--- src/game.js
+++ src/game.js
@@ -2,5 +2,10 @@ class Game {
   constructor() {
     this.player1 = new Player({id: 1, token: "🍺"})
     this.player2 = new Player({id: 2, token: "🍷"})
+    this.playerTurn = this.player1
+  }
+
+  changePlayerTurn() {
+    this.playerTurn = this.playerTurn.id === 1 ? this.player2 : this.player1
   }
 }
